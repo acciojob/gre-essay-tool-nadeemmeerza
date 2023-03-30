9ec3eebb0862cdead826c969385b1065dfd03ec8
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-30 10:33:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+let box = document.getElementById("evaluatedText");
+// document.getElementById("wordCount").innerText = 'change'
+box.addEventListener("input", (e)=>{
+	
+	 const text = box.value.trim();
+  const wordCount = text === '' ? 0 : text.split(/\s+/).length;
+  document.getElementById("wordCount").innerText= wordCount;
+})
